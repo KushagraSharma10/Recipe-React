f304626b9c02ea95a5f95fbb57ff86e0e50ca1ce
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-23 23:53:45 +0530

    display favorite recipes from localStorage in Fav component

--- src/pages/Fav.jsx
+++ src/pages/Fav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import RecipeCard from '../components/RecipeCard'
 
 const Fav = () => {
+  const favorite = JSON.parse(localStorage.getItem("fav"));
+
+  const renderRecipes = favorite.map((recipe) => (
+    <RecipeCard key={recipe.id} recipe={recipe} />
+  ));
+
   return (
-    <div>Fav</div>
-  )
-}
+    <div className="flex flex-wrap">
+      {favorite.length > 0 ? renderRecipes : "No Fav Found!"}
+    </div>
+  );
+};
 
 export default Fav
